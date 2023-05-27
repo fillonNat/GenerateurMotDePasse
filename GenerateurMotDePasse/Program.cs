85fc3d9df3a523d313db7c6064eb4fc65f47432b
Nathalie <fillon_1@hotmail.com>
2023-05-27 17:06:11 -0400

    ajout de la class outils

--- GenerateurMotDePasse/Program.cs
+++ GenerateurMotDePasse/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using FormationCS;
+
 
 namespace MyApp // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
+        
+       
         static void Main(string[] args)
         {
             // 1- Demander la longueur du mot de passe (DemanderNombre) int longeur
+            int longueurMotDePasse = outils.DemanderNombrePositifNonNul("Longeur du mot de passe : ");
             // 2- alphabet 
             // 3- comment choisir un caractère aléatoire
             // 4- generer le mot de passe
---
+++ GenerateurMotDePasse/outils.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormationCS
+{
+    static class outils
+    {
+
+        public static int DemanderNombrePositifNonNul(string question)
+        {
+            return DemanderNombreEntre(question, 1, int.MaxValue);
+        }
+
+        static int DemanderNombreEntre(string question, int min, int max)
+        {
+
+            int nombre = DemanderNombre(question);
+            if ((nombre >= min) && (nombre <= max))
+            {
+                return nombre;
+            }
+
+            Console.WriteLine("ERREUR: Vous devez entre un nombre entre " + min + " et " + max);
+            return DemanderNombreEntre(question, min, max);  // Fonction récursive
+        }
+        static int DemanderNombre(string question)
+        {
+            int reponseInt = 0;
+            while (true)
+            {
+
+                Console.Write(question);
+                string reponse = Console.ReadLine();
+                try
+                {
+                    reponseInt = int.Parse(reponse);
+                    return reponseInt;
+                }
+                catch
+                {
+                    Console.WriteLine("ERREUR: Vous devez rentre un nombre");
+                }
+            }
+        }
+    }
+}
