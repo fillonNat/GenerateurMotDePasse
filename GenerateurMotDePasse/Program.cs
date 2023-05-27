5249862d458cfc717aaca42bd6a02468d41ed776
Nathalie <fillon_1@hotmail.com>
2023-05-27 16:21:25 -0400

    Ajoutez des fichiers projet.

---
+++ GenerateurMotDePasse/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1- Demander la longueur du mot de passe (DemanderNombre) int longeur
+            // 2- alphabet 
+            // 3- comment choisir un caractère aléatoire
+            // 4- generer le mot de passe
+            // 5- Améliorations : choix alphabet
+        }
+    }
+}
